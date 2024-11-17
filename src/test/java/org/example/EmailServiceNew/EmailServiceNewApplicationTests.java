ffdfcdc75c147734dd4a7308e1166b6375893850
Nishant Banginwar <banginwar.nishant1@gmail.com>
2024-11-17 08:19:00 +0530

    Email Service

---
+++ src/test/java/org/example/EmailServiceNew/EmailServiceNewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.EmailServiceNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
